3c69039ac51c9ce7c9522ec44e471ba91c9661a9
Ben <benjaminwpeacock@gmail.com>
2014-05-17 18:44:04 +0000

    Setting unicorn as webserver, adding parse rest api gem, prepping for Heroku deploy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Converter::Application.config.secret_key_base = '767ea98086ddab3f5d53b560f8b3012b1308e7c2c129f988c6131af0eb68f99b2c6aa93638e02affed10590ddf31d707f096663000938e021bb2837e39f40a57'
